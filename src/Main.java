d1ffa9649bf0ca21734c1d4957f36282e87880c4
LSute <l.suter@tljlimited.com>
2023-04-18 09:32:48 +0100

    Tested and download was successful

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String repositoryUrl = "https://github.com/LSuter22/Hardcoded_Auth_Admin/archive/master.zip"; // Replace with your repository URL
+        String accessToken = "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"; // Replace with your GitHub access token
+        String savePath = "/Users/lukesuter/Desktop/repository.zip"; // Replace with the desired save path
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet(repositoryUrl);
+        request.addHeader("Authorization", "Bearer " + accessToken);
+        CloseableHttpResponse response = httpClient.execute(request);
+        HttpEntity entity = response.getEntity();
+        InputStream inputStream = entity.getContent();
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(savePath)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+            }
+            System.out.println("Repository downloaded successfully!");
+        } finally {
+            EntityUtils.consume(entity);
+            response.close();
+            httpClient.close();
+        }
     }
 }
