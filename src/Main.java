daefc0913138c22d964b85a317a991ca3474044c
LSute <l.suter@tljlimited.com>
2023-06-02 11:26:52 +0100

    Tested and download was successful

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +21,8 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Timer;
 
 
 public class Main {
@@ -26,45 +32,99 @@ public class Main {
     ArrayList<String> repoURLList = new ArrayList<>();
     ArrayList<String> savePathList = new ArrayList<>();
 
+    JTextArea TextArea;
+    JTextField RepoURLField;
+    JTextField BranchURLField;
+    JTextField RepoNameField;
 
     public static void main(String[] args) {
         new Main();
     }
 
-    public Main(){
+    protected TitledBorder createTitledBorder(String text) {
+        final TitledBorder border = new TitledBorder(" " + text + " ");
+        border.setTitleFont(new Font("Arial", Font.BOLD, 13));
+        return border;
+    }
+
+    public Main() {
+
+        JFrame frame = new JFrame();
+        JPanel OutputPanel = new JPanel();
+        JPanel addPanel = new JPanel();
+
+        OutputPanel.setBorder(createTitledBorder("Output"));
+        OutputPanel.setLayout(new FlowLayout());
+        OutputPanel.setPreferredSize(new Dimension(1000,750));
+        addPanel.setBorder(createTitledBorder("Add"));
+        addPanel.setLayout(new FlowLayout());
+        addPanel.setPreferredSize(new Dimension(1000,250));
+        frame.setPreferredSize(new Dimension(1010,1030));
+        frame.setLayout(new BorderLayout());
+
+        TextArea = new JTextArea();
+        TextArea.setSize(990,740);
+        TextArea.setLineWrap(true);
+        TextArea.setOpaque(false);
+        TextArea.setBorder(BorderFactory.createEmptyBorder(0,5,0,10));
+        TextArea.setEditable(false);
+
+        RepoURLField = new JTextField();
+        RepoURLField.setPreferredSize(new Dimension(980,25));
+        RepoURLField.setToolTipText("Repo URL");
+
+        BranchURLField = new JTextField();
+        BranchURLField.setPreferredSize(new Dimension(980,25));
+        BranchURLField.setToolTipText("Branch URL");
+
+        RepoNameField = new JTextField();
+        RepoNameField.setPreferredSize(new Dimension(980,25));
+        RepoNameField.setToolTipText("Repo Name");
+
+        JButton addtoconfig = new JButton(new AbstractAction("Add to Backup") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                writeToProperties(RepoNameField.getText(),BranchURLField.getText(),RepoURLField.getText(),"/Users/lukesuter/Desktop/TLJ Qdesk.zip");
+            }
+        });
+
+        OutputPanel.add(TextArea);
+        addPanel.add(RepoNameField);
+        addPanel.add(RepoURLField);
+        addPanel.add(BranchURLField);
+        addPanel.add(addtoconfig);
+
         Timer timer = new Timer();
         Properties properties = new Properties();
 
-        String folderName = "data";
+        File Directory = new File("data");
 
-        Path directoryPath = Paths.get(folderName);
-
-        if (Files.exists(directoryPath) && Files.isDirectory(directoryPath)) {
-            writeToProperties("Test");
-        } else {
-            try {
-                Files.createDirectories(directoryPath);
-                System.out.println("Directory created: " + directoryPath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            writeToProperties("Test");
+        if (!Directory.exists()) {
+            Directory.mkdir();
         }
 
-        getproperties("Test");
+        writeToProperties("Q-Desk","https://api.github.com/repos/TLJ-Group/TLJ_Qdesk/branches/master","https://github.com/TLJ-Group/TLJ_Qdesk/archive/master.zip","/Users/lukesuter/Desktop/TLJ Qdesk.zip");
 
-        String accessToken = "AcessToken"; // GitHub personal access token
+        getRepoNames();
+
+        for (String s: repoNameList){
+            getproperties(s);
+        }
+
+
+        String accessToken = "Access token"; // GitHub personal access token
+
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 System.out.println("Starting Task");
-                for (String repoNameFromList : repoNameList){
+                for (String repoNameFromList : repoNameList) {
                     int index = repoNameList.indexOf(repoNameFromList);
                     try {
-                        if (checkBranch(branchURLList.get(index), accessToken,repoNameFromList, properties)) {
+                        if (checkBranch(branchURLList.get(index), accessToken, repoNameFromList, properties)) {
                             System.out.println("Checked Branch");
                             if (downloadRepository(repoURLList.get(index), accessToken, savePathList.get(index))) {
+                                System.out.println("Downlaoded " + repoNameFromList);
                                 Date date = new Date();
                                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
                                 dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -81,8 +141,7 @@ public class Main {
                     }
                 }
 
-                long nextexec = System.currentTimeMillis() + 30000;
-                // Format the next execution time
+                long nextexec = System.currentTimeMillis() + 900000;
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 String nextExecutionTimeString = sdf.format(new Date(nextexec));
 
@@ -91,9 +150,19 @@ public class Main {
             }
         };
 
-        //Schedule the TimerTask to run every 5 minutes (300000 milliseconds)
-        timer.schedule(task, 0, 30000);
+        //Schedule the TimerTask to run every 15 minutes (900000 milliseconds)
+        timer.schedule(task, 0, 900000);
+
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setTitle("Github Backup");
+        frame.add(OutputPanel,BorderLayout.NORTH);
+        frame.add(addPanel,BorderLayout.SOUTH);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+
     }
+
     public void getproperties(String reponmae) {
 
         try {
@@ -102,10 +171,7 @@ public class Main {
                 if (line.startsWith("branchURLList=")) {
                     String branchURL = line.substring("branchURLList=".length());
                     branchURLList.add(branchURL);
-                } else if (line.startsWith("repoNameList=")) {
-                    String repoName = line.substring("repoNameList=".length());
-                    repoNameList.add(repoName);
-                } else if (line.startsWith("repoURLList=")) {
+                }else if (line.startsWith("repoURLList=")) {
                     String repoURL = line.substring("repoURLList=".length());
                     repoURLList.add(repoURL);
                 } else if (line.startsWith("savePathList=")) {
@@ -124,45 +190,48 @@ public class Main {
         }
     }
 
-    public  void writeToProperties(String reponame){
+    public void getRepoNames(){
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("data\\reponames.txt"));
+            repoNameList.addAll(lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        branchURLList.add("https://api.github.com/repos/LSuter22/Hardcoded_Auth_Admin/branches/master");
+    public void writeToProperties(String reponame,String BranchURL,String repoURL,String savePath) {
 
-        repoNameList.add("Hardcoded_Auth_AdminTest");
-
-        repoURLList.add("https://github.com/LSuter22/Hardcoded_Auth_Admin/archive/master.zip");
-
-        savePathList.add("C:\\Users\\losut\\OneDrive\\backups\\Hardcoded_Auth_Admin.zip");
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data\\" + reponame +".txt"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data\\" + reponame + ".txt"))) {
             // Write branchURLList
-            for (String branchURL : branchURLList) {
-                writer.write("branchURLList=" + branchURL);
+                writer.write("branchURLList=" + BranchURL);
                 writer.newLine();
-            }
 
             // Write repoNameList
-            for (String repoName : repoNameList) {
-                writer.write("repoNameList=" + repoName);
+                writer.write("repoNameList=" + reponame);
                 writer.newLine();
-            }
 
             // Write repoURLList
-            for (String repoURL : repoURLList) {
                 writer.write("repoURLList=" + repoURL);
                 writer.newLine();
-            }
 
             // Write savePathList
-            for (String savePath : savePathList) {
                 writer.write("savePathList=" + savePath);
                 writer.newLine();
-            }
 
             System.out.println("Configuration data written to file.");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data\\reponames.txt"))) {
+            writer.write(reponame);
+            writer.newLine();
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        System.out.println("Configuration data written to file.");
     }
 
 
